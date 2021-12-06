3cc10965a52418a1f5a610b4cf5000b21aa8b176
David Boh <herrboh@gmail.com>
2021-12-06 13:30:08 -0500

    adjust repo

---
+++ variables/target/debug/deps/variables-3b736149aabe80bf.d
@@ -0,0 +1,5 @@
+/run/media/herrboh/Suelta/HERENOW/Rust/learning-rust/variables/target/debug/deps/variables-3b736149aabe80bf: src/main.rs
+
+/run/media/herrboh/Suelta/HERENOW/Rust/learning-rust/variables/target/debug/deps/variables-3b736149aabe80bf.d: src/main.rs
+
+src/main.rs:
